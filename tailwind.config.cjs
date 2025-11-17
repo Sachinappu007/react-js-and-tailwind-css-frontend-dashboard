363c7a489bb532c009b0b3721a39e5df71c21996
Sachinappu007 <sachinappu0707@gmail.com>
2025-11-17 18:12:21 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+content: ['./index.html', './src/**/*.{js,jsx}'],
+theme: {
+extend: {},
+},
+plugins: [],
+}
+
+
+
+
+// postcss.config.cjs
+module.exports = {
+plugins: {
+tailwindcss: {},
+autoprefixer: {},
+},
+}
